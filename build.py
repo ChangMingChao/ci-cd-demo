b417d9cbab29921e5777d6e71884fe6a6d1188f9
ChangMingChao <2431061064@st.btbu.edu.cn>
2026-08-24 18:09:40 +0800

    修复 Windows Jenkins 编码问题：设置 stdout/stderr 为 UTF-8

--- build.py
+++ build.py
@@ -3,6 +3,11 @@
 CI/CD Demo Build Script
 使用 Jinja2 + Faker + emoji 生成演示页面
 """
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
 from jinja2 import Template
 from faker import Faker
 import emoji
